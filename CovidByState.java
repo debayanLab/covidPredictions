dfad4c59abc1a70a9e28ab0fd9a44ca1f742cff8
Dewang Kumar Agarwal <agdewang@outlook.com>
2020-06-14 17:10:48 +0530

    Updated and ready for submission
    Updated with documentation and parameters

--- CovidByState.java
+++ CovidByState.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
 public class CovidByState {
+    /**
+     * Converts each of line of the file which is extracted as a String into an array. 
+     * @param n a line from the input file
+     * @return a array of the comma separated values in n 
+     */
     public static String[] clean_output(String n) {
         String[] input = new String[4];
         Scanner sc1 = new Scanner(n);
@@ -15,10 +20,15 @@ public class CovidByState {
         }
         return input;
     }
+    
+    /**
+     * Takes input file and stores each datapoint as an array entry.
+     * @return an array of data from the input file
+     */
     public static ArrayList<String[]> input() {
         ArrayList<String[]> inputData = new ArrayList<>();
         try {
-            File file = new File("Covid Data.csv");
+            File file = new File("Covid National Data Updated.csv");
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 inputData.add(clean_output(sc.nextLine()));
@@ -26,13 +36,35 @@ public class CovidByState {
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
         } catch (Exception e) {
-            System.out.println("Some error occured");
+            System.out.println(e);
         }
         return inputData;
     }
+    
+    /**
+     * Gives the prediction for the reported cases in India by summing state predictions
+     * @param n the number of days for which the prediction must be given
+     * @return the list of prediction of total cases for n days
+     */
+    public static long[] getPrediction(HashMap<String, State> states, int n) {
+        long total[] = new long[n];
+        for (State i : states.values()) {
+            long[] byState = i.getPrediction(n);
+            for(int j = 0; j < n;j++) {
+                total[j] += byState[j];
+            }
+        }
+        return total;
+    }
+    
+    /**
+     * Calls all functions and gets predictions and adjusts for randomness
+     * @param args the number of days for which the national prediction is to be given
+     */
     public static void main(String[] args) {
         long count = 0;
-        while (count / 100 != 185) {
+        //while loop below uses first test data to correct for randomness while assigning districts
+        while (count / 1000 != 143) {//25th March has between 143k to 144k reported cases
             ArrayList<String[]> data = input();
             HashMap <String, State> states = new HashMap<>();
             for (String[] i : data) {
@@ -43,36 +75,46 @@ public class CovidByState {
             }
             long[] prediction = getPrediction(states, Integer.parseInt(args[0]));
             count = prediction[0];
-            if (count / 100 == 185)
+            if (count / 1000 == 143) {
                 for(long i : prediction) {
                     System.out.println(i);
                 }
+               break;
+             }
+             /*
+              * Can be uncommented to see what is the first value reported.
+              else {
+                 System.out.println(count);
+              }
+              */
         }
-    }
-    public static long[] getPrediction(HashMap<String, State> states, int n) {
-        long total[] = new long[n];
-        for (State i : states.values()) {
-            long[] byState = i.getPrediction(n);
-            for(int j = 0; j < n;j++) {
-                total[j] += byState[j];
-            }
-        }
-        //System.out.println("National");
-        return total;
     }
 }
+
+/** 
+ * Represents a State in India
+ */
 class State {
     HashMap<String, District> districts;
     String name;
     long total_cases;
+    /**
+     * Creates a new State with the given name and other default values
+     * @param name the name of the State
+     */
     public State(String n) {
         districts = new HashMap<>();
         this.name = n;
         total_cases = 0;
     }
     
+    /**
+     * Adds a case to the district of the state
+     * @param n the name of the district
+     * @param date the date on which the case is reported
+     */
     public void addDistrict(String n, String date) {
-        if (dateValue(date) > 94)
+        if (dateValue(date) > 99)
         return;
         if (n.equals("")) {
             n = randomlyAssignDistrict();
@@ -85,6 +127,10 @@ class State {
         districts.get(n).addDate(date);
     }
     
+    /**
+     * When no district is mentioned, performs a weighted random assignment of the case to a district in the state
+     * @return the name of the district to which the case is assigned
+     */
     public String randomlyAssignDistrict() {
         double random = Math.random();
         String districtName = "";
@@ -100,6 +146,11 @@ class State {
         return districtName;
     }
     
+    /**
+     * Gives the prediction for the reported cases in the state by summing district predictions
+     * @param n the number of days for which the prediction must be given
+     * @return the list of prediction of total cases for n days
+     */
     public long[] getPrediction(int n) {
         long total[] = new long[n];
         for (District i: districts.values()) {
@@ -108,37 +159,41 @@ class State {
                 total[j] += byDistrict[j];
             }
         }
-        /*for (District i: districts.values()) {
-            if (i.check()) {
-                System.out.println(i.name);
-                System.out.println(i.total_cases);
-                System.out.println(Arrays.toString(i.prediction(n)));
-                System.out.println(i.mean + " " + i.x_not);
-            }
-        }*/
         return total;
     }
+    
+    /**
+     * Helper function to convert date to integer value representing number of days after 30/1/2020
+     * @param date the date to be converted to integer
+     * @return the integer value of the date
+     */
     private int dateValue(String date) {
         int []months = {31,60,91,121,152,182,213,244,274,305,335,366};
         int day = Integer.parseInt(date.substring(0,2));
         int month = Integer.parseInt(date.substring(3,5));
         if (month == 0) { 
-            return day - 30; //since first date was 30/01/20.
+            return day - 30;
         } else {
             return months[month - 1] + day - 30;
         }
     }
 }
+
+/**
+ * Represents a District in India
+ */
 class District {
-    double probability_of_positive = 8; 
-    double coefficient_of_movement = 9;//in lockdown conditions
-    final static int data_for_days = 96;
+    final static int data_for_days = 100;
     double x_not;
     double mean;
     String name;
     int total_cases;
     ArrayList<Integer> cases_and_projections;
     int[] date_count;
+    /**
+     * Creates a new District with the given name and other default values
+     * @param name the name of the district
+     */
     public District(String name) {
         x_not = mean = 0;
         this.name = name;
@@ -146,22 +201,41 @@ class District {
         cases_and_projections = new ArrayList<>();
         date_count = new int[400];
     } 
+    
+    /**
+     * Increases the total number of cases on date by 1.
+     * @param date the date at which a case occured
+     */
     public void addDate(String date) {
         date_count[dateValue(date)] += 1;
     }
+    
+    /**
+     * Increases total cases in district by 1
+     */
     public void tick_total() {
         this.total_cases += 1;
     }
+    
+    /**
+     * Helper function to convert date to integer value representing number of days after 30/1/2020
+     * @param date the date to be converted to integer
+     * @return the integer value of the date
+     */
     private int dateValue(String date) {
         int []months = {31,60,91,121,152,182,213,244,274,305,335,366};
         int day = Integer.parseInt(date.substring(0,2));
         int month = Integer.parseInt(date.substring(3,5));
         if (month == 0) { 
-            return day - 30; //since first date was 30/01/20.
+            return day - 30;
         } else {
             return months[month - 1] + day - 30;
         }
     }
+    
+    /**
+     * Calculates the Gaussian mean and x_not value using historic data
+     */
     public void analyse() {
         double[] mean1 = new double[cases_and_projections.size()];
         int pointer = 0;
@@ -182,35 +256,35 @@ class District {
         }
         mean = x / pointer;
     }
+    
+    /**
+     * Calculates the value of mu for each iteration (see Appendix A)
+     * @param x the current day (x-coordinate)
+     * @param fx the number of reported cases on day x
+     * @param fm the slope of the graph at x
+     * @param fpp the rate of change of slope of the graph
+     * @return the value mu for the given parameters
+     */
     public double getMean(int x, double fm, int fx, double fpp) {
         return - fx * fm + x * fx * fpp - x * fm * fm;
     }
-    public long[] prediction (int n) {
-        long[] y = new long[n];
-        double spread_factor = 1;
-        getCases();
-        int limit = 90;
-        if(check()) {
-            analyse();
-            for (int i = 0; i < n; i++) {
-                double x = (cases_and_projections.size() + i - 25) / (2 * x_not);
-                if (i > 16) {
-                    if (coefficient_of_movement < limit)
-                    coefficient_of_movement += Math.sqrt(limit) / 6.0;
-                }
-                spread_factor = Math.sqrt(coefficient_of_movement);
-                y[i] = Math.round (18.5 * Math.sqrt(probability_of_positive) * mean * sigmoid(x) * spread_factor) - 584;
-            }
-        } else {
-            for(int i = 0; i < n; i++) {
-                y[i] = cases_and_projections.get(cases_and_projections.size() - 1);
-            }
+    
+    
+    /**
+     * Extracts cases by date and creates a list with total cases till each day. 
+     */
+    public void getCases() {
+        int sum = date_count[0];
+        for (int i = 1; i <= data_for_days; i++) {
+            sum += date_count[i];
+            if(sum != 0)
+            cases_and_projections.add(sum);
         }
-        return y;
     }
-    public double sigmoid(double x) {
-        return 1 / (1 + Math.exp(-x));
-    }
+    
+    /**
+     * @return whether a district has had at least 5 days where new cases were reported
+     */
     public boolean check() {
         int count = 0;
         for (int i = 0; i < 400; i++) {
@@ -224,12 +298,35 @@ class District {
             return true;
         }
     }
-    public void getCases() {
-        int sum = date_count[0];
-        for (int i = 1; i <= data_for_days; i++) {
-            sum += date_count[i];
-            if(sum != 0)
-            cases_and_projections.add(sum);
+    
+    /**
+     * Gives the prediction of reported total cases for the district
+     * @param n the number of days for which the prediction is to be given
+     * @return the prediction of total reported cases for the n days in the district
+     */
+    public long[] prediction (int n) {
+        long[] y = new long[n];
+        getCases();
+        if(check()) {
+            analyse();
+            for (int i = 0; i < n; i++) {
+                double x = (cases_and_projections.size() + i - 70) / (4.65 * x_not);
+                y[i] = Math.round (1180 * mean * sigmoid(x)) - 5100;
+            }
+        } else {
+            for(int i = 0; i < n; i++) {
+                y[i] = cases_and_projections.get(cases_and_projections.size() - 1);
+            }
         }
+        return y;
+    }
+    
+    /**
+     * Returns the value of sigmoid(x)
+     * @param x value for which sigmoid needs to be given.
+     * @return the value of sigmoid(x)
+     */
+    public double sigmoid(double x) {
+        return 1 / (1 + Math.exp(-x));
     }
 }
